b6d48dbb88888b274732d528359e708a0efe4e8a
Ivy Rogatko <ipr210lehigh@gmail.com>
2016-09-10 20:32:29 -0700

    gulp notify for growl

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const es = require('event-stream')
 const glob = require('glob')
 const gulp = require('gulp')
 const livereload = require('gulp-livereload')
+const notify = require('gulp-notify')
 const rename = require('gulp-rename')
 const runSequence = require('run-sequence')
 const source = require('vinyl-source-stream')
@@ -13,7 +14,7 @@ const transform = require('vinyl-transform')
 
 function handleError(err) {
   console.log(err.toString())
-  this.emit('end')
+  return 'Gulp Error! ' + err.toString()
 }
 
 gulp.task('browserify', done => {
@@ -23,8 +24,9 @@ gulp.task('browserify', done => {
     const tasks = files.map(entry => {
       return browserify({ entries: [entry] })
           .bundle()
+          .on('error', notify.onError(handleError))
           .pipe(source(entry))
-          .on('error', handleError)
+          .on('error', notify.onError(handleError))
           .pipe(rename({
               extname: '.bundle.js'
           }))
@@ -43,7 +45,7 @@ gulp.task('js-compile', () => {
       output_wrapper: '(function(){\n%output%\n}).call(this)',
       js_output_file: 'build.min.js'
     }))
-    .on('error', handleError)
+    .on('error', notify.onError(handleError))
     .pipe(gulp.dest('./dist/build/js'))
 })
 
@@ -57,26 +59,26 @@ gulp.task('babel', () =>
       "transform-class-properties"
     ]
   }))
-  .on('error', handleError)
+  .on('error', notify.onError(handleError))
   .pipe(gulp.dest('./dist/js'))
 )
 
 gulp.task('reload-js', input => {
- return gulp.src('./dist/**/*.js').pipe(livereload())
+ return gulp.src('./dist/**/*.js').pipe(livereload()).on('error', notify.onError(handleError))
 })
 
 gulp.task('reload-pug', input => {
- return gulp.src('./client/views/pages/*.pug').pipe(livereload())
+ return gulp.src('./client/views/pages/*.pug').pipe(livereload()).on('error', notify.onError(handleError))
 })
 
 gulp.task('clean', () => {
   return del([
     './dist/js/**/*.js',
-  ]);
+  ])
 })
 
 gulp.task('js-pipeline', callback => {
-  runSequence('clean', 'babel', 'browserify', 'reload-js', callback);
+  runSequence('clean', 'babel', 'browserify', 'reload-js', callback)
 })
 
 gulp.task('watch', () => {
